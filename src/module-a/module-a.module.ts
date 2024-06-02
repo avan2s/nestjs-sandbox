d6e8c983f6462d0cfb7abc0040692011d97c21a9
avh <andreas.vanhelden@conceto.de>
2024-06-02 19:18:25 +0200

    main: use factory for dependency injection

--- src/module-a/module-a.module.ts
+++ src/module-a/module-a.module.ts
@@ -7,6 +7,8 @@ import {
 } from './services/service-a2/service-a2.service';
 import { ServiceA2Token } from './interfaces';
 import { AbstractServiceA2 } from './abstracts/abstract-service-a2';
+import { Person } from './types';
+import { ModuleRef } from '@nestjs/core';
 
 @Module({
   providers: [
@@ -22,6 +24,21 @@ import { AbstractServiceA2 } from './abstracts/abstract-service-a2';
     {
       provide: AbstractServiceA2,
       useClass: ServiceA2ServiceImpl3,
+    },
+    {
+      provide: 'Person',
+      useFactory() {
+        return { age: 20, name: 'bar' } satisfies Person;
+      },
+    },
+    {
+      provide: 'Persons',
+      useFactory(moduleRef: ModuleRef) {
+        const abstr = moduleRef.get(AbstractServiceA2);
+        const result = abstr.abstractMethod1(20);
+        return result;
+      },
+      inject: [ModuleRef],
     },
   ],
 })
--- src/module-a/services/service-a1/service-a1.service.ts
+++ src/module-a/services/service-a1/service-a1.service.ts
@@ -1,15 +1,20 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { AbstractServiceA2 } from 'src/module-a/abstracts/abstract-service-a2';
 import { ServiceA2, ServiceA2Token } from 'src/module-a/interfaces';
+import { Person } from 'src/module-a/types';
 
 @Injectable()
 export class ServiceA1Service {
   constructor(
     @Inject(ServiceA2Token) private readonly serviceA2: ServiceA2,
     private readonly abstractServiceA2: AbstractServiceA2,
+    @Inject('Person') person: Person,
+    @Inject('Persons') persons: Person[],
   ) {
-    console.log(this.serviceA2.method1({ age: 20, name: 'Peter' }));
-    console.log(abstractServiceA2.abstractMethod1(2));
+    console.log(this.serviceA2);
+    console.log(abstractServiceA2);
+    console.log(person);
+    console.log(persons);
   }
 
   public bar() {
